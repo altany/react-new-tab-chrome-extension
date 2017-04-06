4817a0b3ddd92a45c3c4103749cd913473a25636
Tania Papazafeiropoulou <taniapapazaf@gmail.com>
2017-04-06 12:54:09 +0100

    restructure the popup state

--- app/containers/Popup.js
+++ app/containers/Popup.js
@@ -1,33 +1,67 @@
 import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import style from './App.css';
 
 @connect(
-  (state) => state.popup
+  (state) => {
+    let selected;
+    if (!state.popup.open) selected=false;
+    if (state.popup.mode==='section') {
+      selected = state.sections.filter(section =>
+        section.id === state.popup.id
+      )[0];
+    }
+    else if (state.popup.mode==='bookmark') {
+      selected = state.bookmarks.filter(bookmark =>
+        bookmark.id === state.popup.id
+      )[0];
+    }
+    return {
+      mode: state.popup.mode,
+      selected
+    };
+  }
 )
 
 export default class Popup extends Component {
   static propTypes = {
-    open: PropTypes.bool,
     mode: PropTypes.oneOf(['section', 'bookmark']),
-    id: PropTypes.number,
-    position: PropTypes.object
+    selected: PropTypes.oneOfType([
+      React.PropTypes.bool,
+      React.PropTypes.object
+    ])
   };
 
   constructor(props) {
     super(props);
   }
 
+  renderSection() {
+    return(
+      <div className={style.popup}>
+        {this.props.mode} id:{this.props.selected.id}
+      </div>
+    );
+  }
+
+  renderBookmark() {
+    return(
+      <div className={style.popup}>
+        {this.props.mode} id:{this.props.selected.id}
+      </div>
+    );
+  }
+
   render() {
-    console.log(this.props);
-    if (!this.props.open) return null;
-    return <div style={{
-      display: 'block',
-      width: 400,
-      height: 200,
-      backgroundColor: 'pink',
-      position: 'absolute',
-      right: 0
-    }}>{this.props.mode} id:{this.props.id} </div>;
+    console.log(this.props)
+    if (!this.props.selected) return null;
+    if (this.props.mode==='section') {
+      return this.renderSection()
+    }
+    else if (this.props.mode==='bookmark') {
+      return this.renderBookmark()
+    }
+    return null;
   }
 }
